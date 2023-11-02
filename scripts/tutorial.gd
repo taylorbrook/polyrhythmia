645162f3f646f9cb042b9caf5c11ffa5c636cb40
taylorbrook <taylor.brook@gmail.com>
2023-11-02 19:36:47 -0400

    added 2d path and working on setting up variables

---
+++ scripts/tutorial.gd
@@ -0,0 +1,42 @@
+extends Node2D
+@export var bpm: float = 60
+@export var subdiv_r: int = 4
+@export var subdiv_l: int = 4
+
+var beatsignal_r: int
+var beatsignal_l: int
+var measuresignal: float
+var measurecount: int
+
+var beat1trigger_r = false
+var beat2trigger_r = false
+var beat3trigger_r = false
+var beat4trigger_r = false
+
+#button inputs "D" and "J"
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("button1"):
+		$button1.play()
+	if Input.is_action_just_pressed("button2"):
+		$button2.play()
+
+	var mstime: float = Time.get_ticks_msec()
+	var beatsignal_r_float: float = (mstime / 1000) / (bpm / 60)
+	var beatsignal_r = int(beatsignal_r_float) % subdiv_r + 1
+	
+	# calculate measures
+	var measuresignal: float = (mstime / 1000) / (bpm / 60) / 4
+	var measurecount = int(measuresignal)
+
+	# right trigger subdivisions
+	if(beatsignal_r >= 1 and beat1trigger_r == false):
+		beat1trigger_r = true
+	if(beatsignal_r >= 2 and beat1trigger_r == false):
+		beat2trigger_r = true
+	if(beatsignal_r >= 3 and beat1trigger_r == false):
+		beat3trigger_r = true
+	if(beatsignal_r >= 4 and beat1trigger_r == false):
+		beat4trigger_r = true
+			
+	#debug
+	#print(beatsignal_r)

dbc4732f90283e599973373c0b6bd83f600f641f
taylorbrook <taylor.brook@gmail.com>
2023-11-05 14:19:38 -0500

    exposed second delay tap time, merged in updates from joe branch

--- scripts/delayproperties.gd
+++ scripts/delayproperties.gd
@@ -1,18 +1,30 @@
 extends Node
 
-@export var delaytap1 = 0.0 : set = _delay_setter, get = _delay_getter 
+@export var delaytap1 = 0.0 : set = _delaytap1_setter, get = _delaytap1_getter
+@export var delaytap2 = 0.0 : set = _delaytap2_setter, get = _delaytap2_getter
 
 func _ready():
-	_delay_getter()
+	_delaytap1_getter()
+	_delaytap2_getter()
 
-func _delay_setter(val):
+func _delaytap1_setter(val):
 	#when your variable is 'set', this code is executed.
 	delaytap1 = val
 	AudioServer.get_bus_effect(1,0).tap1_delay_ms = val
 	
-func _delay_getter():
+func _delaytap2_setter(val):
+	#when your variable is 'set', this code is executed.
+	delaytap2 = val
+	AudioServer.get_bus_effect(1,0).tap2_delay_ms = val
+	
+func _delaytap1_getter():
 	#when your variable is retrieved, this code is executed.
 	return AudioServer.get_bus_effect(1,0).tap1_delay_ms
+	
+func _delaytap2_getter():
+	#when your variable is retrieved, this code is executed.
+	return AudioServer.get_bus_effect(1,0).tap2_delay_ms
 
 func _process(delta):
 	print(delaytap1)
+	print(delaytap2)

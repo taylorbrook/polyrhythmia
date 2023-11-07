e7d8602f87d6d74b03996019c2e41c4274db65a3
taylorbrook <taylor.brook@gmail.com>
2023-11-07 16:57:34 -0500

    added a cmoothing value (lerp) to the audioreactive texture

--- rhythms/audio_spectrum_helper.gd
+++ rhythms/audio_spectrum_helper.gd
@@ -5,12 +5,15 @@ const VU_COUNT = 12
 const FREQ_MAX = 10000.0
 const MIN_DB = 66
 
+@export var lerp_smoothing: float = 4
+var lerped_spectrum: Array[float] = []
 var spectrum
 var image_texture: ImageTexture
 var image: Image
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	lerped_spectrum.resize(VU_COUNT)
 	image = Image.create(VU_COUNT,1,false, Image.FORMAT_RGBA8)
 	for i in VU_COUNT:
 		image.set_pixel(i,0, Color.BLACK)
@@ -23,12 +26,13 @@ func _ready():
 func _process(delta: float) -> void:
 	var prev_hz = 0
 	
-	for i in range(1, VU_COUNT):
+	for i in range(1, VU_COUNT+1):
 		var hz = i * FREQ_MAX / VU_COUNT;
 		var magnitude: float = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
 		var energy = clamp((MIN_DB + linear_to_db(magnitude)) / MIN_DB, 0, 1)
 		var effect = energy * Multiplier
-		image.set_pixel(i,0, Color.WHITE * effect)
+		lerped_spectrum[i-1] = lerp(lerped_spectrum[i-1],effect,delta * 4)
+		image.set_pixel(i-1,0, Color.WHITE * lerped_spectrum[i-1])
 		prev_hz = hz
 		
 	image_texture.update(image)

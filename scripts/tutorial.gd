190a2417dcf9bf2fb39f2d37ddfd79a2524640b2
taylorbrook <taylor.brook@gmail.com>
2023-11-15 18:17:16 -0500

    added a few more samples. new song begun, but it crashes when I try to run it

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -2,6 +2,3 @@ extends Node2D
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://levels/3d_test.tscn")
-
-func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://rhythms/audioreactivetexture.tscn")

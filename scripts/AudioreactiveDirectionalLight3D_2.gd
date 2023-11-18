70ad5c8ca7503b8e938fd44ecd6c633ee70a1894
taylorbrook <taylor.brook@gmail.com>
2023-11-18 12:58:13 -0500

    added more to the song animations. tweaked lighting - added extra light

--- scripts/AudioreactiveDirectionalLight3D_2.gd
+++ scripts/AudioreactiveDirectionalLight3D_2.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	var spectrum3 = abs(Globals.lerped_spectrum[5])
 	#var amplitude = AudioServer.get_bus_peak_volume_right_db(0,0)
 	light_energy = .15 + spectrum1 / 3
-	var red = .3 + spectrum1 / 2
-	var green = .2 + spectrum2 / 2
-	var blue = .2 + spectrum3 / 2
+	var red = .1 + spectrum1 / 2
+	var green = .4 - spectrum2 / 2
+	var blue = 1 - spectrum3 / 2
 	light_color = Color(red, green, blue)

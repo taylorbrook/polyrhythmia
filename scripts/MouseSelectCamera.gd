528f6a13a893b503191d58955ad86130bd98ddf2
taylorbrook <taylor.brook@gmail.com>
2023-11-22 18:01:57 -0500

    mouse hover function added to the menu

--- scripts/MouseSelectCamera.gd
+++ scripts/MouseSelectCamera.gd
@@ -9,6 +9,94 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			raycast_from_mouse(m_pos, 1)
 
+func _process(delta):
+	m_pos = get_viewport().get_mouse_position()
+	raycast_from_mouse_hover(m_pos, 1)
+
+#raycast on hover
+func raycast_from_mouse_hover(m_pos, collision_mask):
+	var ray_start = project_ray_origin(m_pos)
+	var ray_end = ray_start + project_ray_normal(m_pos) * 100
+	var world3d : World3D = get_world_3d()
+	var space_state = world3d.direct_space_state
+
+	if space_state == null:
+		return
+
+	var query = PhysicsRayQueryParameters3D.create(ray_start, ray_end, collision_mask)
+	query.collide_with_areas = true
+	var result = space_state.intersect_ray(query)
+
+	if result.is_empty():
+		$"../LevelWheel/Trigger1".transparency = 0
+		$"../LevelWheel/Trigger2".transparency = 0
+		$"../LevelWheel/Trigger3".transparency = 0
+		$"../LevelWheel/Trigger4".transparency = 0
+		$"../LevelWheel/Trigger5".transparency = 0
+		$"../LevelWheel/Trigger6".transparency = 0
+		$"../LevelWheel/Trigger7".transparency = 0
+		$"../LevelWheel/Trigger8".transparency = 0
+		$"../TempoWheel/Trigger1".transparency = 0
+		$"../TempoWheel/Trigger2".transparency = 0
+		$"../TempoWheel/Trigger3".transparency = 0
+		$"../TempoWheel/Trigger4".transparency = 0
+		$"../TempoWheel/Trigger5".transparency = 0
+		$"../TempoWheel/Trigger6".transparency = 0
+		$"../TempoWheel/Trigger7".transparency = 0
+		$"../TempoWheel/Trigger8".transparency = 0
+		return
+		
+	#level select wheel buttons
+	if result.collider == %TriggerShape1:
+		$"../LevelWheel/Trigger1".transparency = .5
+
+	if result.collider == %TriggerShape2:
+		$"../LevelWheel/Trigger2".transparency = .5
+
+	if result.collider == %TriggerShape3:
+		$"../LevelWheel/Trigger3".transparency = .5
+
+	if result.collider == %TriggerShape4:
+		$"../LevelWheel/Trigger4".transparency = .5
+
+	if result.collider == %TriggerShape5:
+		$"../LevelWheel/Trigger5".transparency = .5
+
+	if result.collider == %TriggerShape6:
+		$"../LevelWheel/Trigger6".transparency = .5
+
+	if result.collider == %TriggerShape7:
+		$"../LevelWheel/Trigger7".transparency = .5
+
+	if result.collider == %TriggerShape8:
+		$"../LevelWheel/Trigger8".transparency = .5
+		
+	#tempo select wheel
+	if result.collider == %TempoTrigger1:
+		$"../TempoWheel/Trigger1".transparency = .5
+
+	if result.collider == %TempoTrigger2:
+		$"../TempoWheel/Trigger2".transparency = .5
+
+	if result.collider == %TempoTrigger3:
+		$"../TempoWheel/Trigger3".transparency = .5
+
+	if result.collider == %TempoTrigger4:
+		$"../TempoWheel/Trigger4".transparency = .5
+
+	if result.collider == %TempoTrigger5:
+		$"../TempoWheel/Trigger5".transparency = .5
+
+	if result.collider == %TempoTrigger6:
+		$"../TempoWheel/Trigger6".transparency = .5
+
+	if result.collider == %TempoTrigger7:
+		$"../TempoWheel/Trigger7".transparency = .5
+
+	if result.collider == %TempoTrigger8:
+		$"../TempoWheel/Trigger8".transparency = .5
+	
+#raycast on click
 func raycast_from_mouse(m_pos, collision_mask):
 	var ray_start = project_ray_origin(m_pos)
 	var ray_end = ray_start + project_ray_normal(m_pos) * 100

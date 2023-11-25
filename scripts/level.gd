d1f4d105c2074623a3d1c510f36cc791b6bdb67b
taylorbrook <taylor.brook@gmail.com>
2023-11-25 01:03:39 -0500

    added the basics for a score system and UI overlay

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,13 +4,27 @@ extends Node3D
 @export var Fog = 0.0 : set = fog_setter
 var animation_speed: float
 
+var bpm_multiplier = Globals.bpm/30.
+
 func _ready():
 	$BigWheels/MainCam/Camera3D/AnimationPlayer.play("entry")
-	animation_speed = Globals.bpm/30.
 	await get_tree().create_timer(4.0).timeout
-	$SongPlayer.play(SongName, 0, animation_speed)
+	$SongPlayer.play(SongName, 0, bpm_multiplier)
+	_update_score(0)
+	var tween = create_tween().set_loops()
+	tween.tween_property($BigWheels/OmniLight3D, "light_energy", 1.3, bpm_multiplier).from_current().set_ease(Tween.EASE_OUT)
 
 func fog_setter(val):
 	if is_instance_valid($Backdrop/WorldEnvironment):
 		var env:Environment = $Backdrop/WorldEnvironment.environment
 		env.volumetric_fog_density=val
+
+func _on_song_player_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://levels/menu.tscn")
+	Sound.transition_start()
+
+func _update_score(val):
+	var tween = create_tween().set_loops()
+	tween.tween_property($ui/score, "scale", Vector2(1.1,1.1), bpm_multiplier).from_current().set_ease(Tween.EASE_OUT)
+	$ui/score.text = str(val)
+	
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,7 +5,6 @@ func _ready():
 	$AnimationPlayer.play("cameraSway")
 	var tween = create_tween().set_loops()
 	tween.tween_property($LevelWheel/levelName, "scale", Vector3(1.1,1.1,1.1), 3/2).from_current().set_ease(Tween.EASE_OUT)
-	#tween.tween_property($LevelWheel/levelName, "scale", Vector3(.95,.95,.95), .5).from_current().set_ease(Tween.EASE_IN)
 
 
 func _on_button_pressed():

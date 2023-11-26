e5d75f1a916bf6487ee1abf15d92d8c4fa11d844
taylorbrook <taylor.brook@gmail.com>
2023-11-26 17:42:45 -0500

    implementation of score system inside the level.gd script

--- scripts/level.gd
+++ scripts/level.gd
@@ -5,12 +5,25 @@ extends Node3D
 var animation_speed: float
 
 var bpm_multiplier = Globals.bpm/30.
+var score
+var scoreFeedback = " "
+var combo_counter
+
+#func _process(delta):
+#	#for testing score system
+#	if Input.is_key_pressed(KEY_A):
+#		_perfect()
+#	if Input.is_key_pressed(KEY_S):
+#		_good()
+#	if Input.is_key_pressed(KEY_D):
+#		_miss()
 
 func _ready():
+	_reset_score()
+	_update_score(0)
 	$BigWheels/MainCam/Camera3D/AnimationPlayer.play("entry")
 	await get_tree().create_timer(4.0).timeout
 	$SongPlayer.play(SongName, 0, bpm_multiplier)
-	_update_score(0)
 	var tween = create_tween().set_loops()
 	tween.tween_property($BigWheels/OmniLight3D, "light_energy", 1.3, bpm_multiplier).from_current().set_ease(Tween.EASE_OUT)
 
@@ -23,8 +36,34 @@ func _on_song_player_animation_finished(anim_name):
 	get_tree().change_scene_to_file("res://levels/menu.tscn")
 	Sound.transition_start()
 
+#score system - takes value and scales with bpm mulitplier and combo counter
+func _reset_score():
+	score = 0
+	combo_counter = 0
+	scoreFeedback = " "
+	
 func _update_score(val):
 	var tween = create_tween().set_loops()
 	tween.tween_property($ui/score, "scale", Vector2(1.1,1.1), bpm_multiplier).from_current().set_ease(Tween.EASE_OUT)
-	$ui/score.text = str(val)
+	score += val*bpm_multiplier*(combo_counter*.5)
+	$ui/score.text = str(score)
+	$ui/scoreFeedback.text = scoreFeedback + "combo x" + str(combo_counter)
+
+func _perfect():
+	scoreFeedback = "perfect!"
+	_update_score(10)
+	combo_counter += 1
+	pass
 	
+func _good():
+	scoreFeedback = "good!"
+	_update_score(5)
+	combo_counter += 1
+	pass
+
+func _miss():
+	scoreFeedback = "miss!"
+	_update_score(0)
+	combo_counter = 0
+	
+	pass
